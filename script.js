7477d2de28425e743bd5cbd9e470fc25edb92e22
eynervaldez <164020546+eynervaldez@users.noreply.github.com>
2024-10-26 15:03:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+var nombre = document.querySelector("#nombre")
+
+
+
+
+
+
+function saludar(){
+    alert("saludos! como te encuentras hoy?")
+}
+
+
+function cambioColor(){
+    nombre.style.color="red"
+}
+
+function esconder(elemento){
+    elemento.remove()
+}
